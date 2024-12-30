b50d8a9c0d1672780c6918d097ee493eebefd509
joaopedroa <joaopedroa.6@hotmail.com>
2024-12-30 12:57:29 -0300

    Atualizando codigo auxiliar python

--- massas/codigo_python_auxiliar.py
+++ massas/codigo_python_auxiliar.py
@@ -6,6 +6,11 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 import json
+from datetime import datetime
+from awsglue import DynamicFrame
+from pyspark.sql import functions as F
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -39,6 +44,52 @@ path_arquivo_name = '/'.join(path_arquivo_array)
 
 print(f"Bucket name is {bucket_name}")
 print(f"Path name is {path_arquivo_name}")
+
+dynamicFrame = glue_context.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="csv",
+    connection_options={"paths": ['s3://files-joao/SF/'], "recurse": False},
+    transformation_ctx="transformation_ctx111")
+
+data_frame = dynamicFrame.toDF()
+
+data_frame.show(2)
+print("printouuu")
+
+to_data_udf = udf(lambda: datetime.today().strftime('%Y-%m-%d'), StringType())
+
+column_base = 'col0'
+
+data_frame2 = data_frame.withColumn('custodia', col(column_base).substr(0, 2)).withColumn('id_carga',
+                                                                                          col(column_base).substr(3,
+                                                                                                                  2).cast(
+                                                                                              "Integer")).withColumn(
+    'id_operacao', col(column_base).substr(5, 9).cast("Long")).withColumn('data_versao', to_data_udf())
+
+data_frame2.show(10)
+
+dynamicFrame = DynamicFrame.fromDF(data_frame2, glue_context, "transformation_ctx_2")
+
+dynamicFrame = ApplyMapping.apply(frame=dynamicFrame, mappings=[("custodia", "string", "custodia", "string"),
+                                                                ("id_operacao", "Long", "id_operacao", "bigint"),
+                                                                ("id_carga", "Integer", "id_carga", "string"),
+                                                                ("data_versao", "string", "data_versao", "string")
+                                                                ], transformation_ctx="transformation_ctx111")
+
+glue_context.write_dynamic_frame_from_catalog(
+    frame=dynamicFrame,
+    database="pos-venda",
+    table_name='tb_operacoes',
+    transformation_ctx="write_sink",
+    additional_options={
+        "enableUpdateCatalog": True,
+        "updateBehavior": "LOG",
+        "partitionKeys": ["data_versao", "id_carga"],
+        "compression": "snappy"
+    }
+)
+
+print('democratizouuu')
 
 df2 = glue_context.create_dynamic_frame.from_options(
     connection_type="s3",
